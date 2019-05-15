9f881d3bc26c91b2ed023d7167ea76fbd0cb3366
Ian Rey Baguio <ronanzter@yahoo.com>
2019-05-14 23:48:48 -0600

    Added Stepper to Register component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Switch, Route, Redirect, withRouter, Link} from
 import './App.css';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
-import Register from './components/Register';
+import Demo from './components/Register';
 import Layout from './components/Layout';
 import ProjectTable from './components/ProjectTable';
 
@@ -16,7 +16,7 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={Login}/>
-            <Route exact path="/register" component={Register}/>
+            <Route exact path="/register" component={Demo}/>
             <PrivateRoute exact path="/main" component={ProjectTable} />
             <Redirect to="/" />
           </Switch>
--- src/components/Register.js
+++ src/components/Register.js
@@ -8,10 +8,13 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
+//reference: https://material-ui.com/
+//tutorial: https://codesandbox.io/s/rlv2r08lno
 const styles = theme => ({
     root:{
-        width: '90%',
+        width: '100%',
     },
     button: {
         marginRight: theme.spacing.unit,
@@ -40,11 +43,127 @@ function getStepContent(step){
 }
 
 class Register extends React.Component{
+    state = {
+        activeStep: 0,
+        firstName: "",
+        lastName: "",
+        username: "",
+        password: ""
+    }
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+      };
+
+    handleNext = () => {
+        this.setState(state => ({
+            activeStep: state.activeStep + 1,
+        }));
+
+        console.log("First Name:" + this.state.firstName);
+        console.log("Last Name:" + this.state.lastName);
+    };
+
+    handleBack = () => {
+        this.setState(state => ({
+            activeStep: state.activeStep - 1,
+        }));
+    };
+
+    handleReset = () => {
+        this.setState({
+            activeStep: 0,
+        });
+    };
+
     render(){
+        const {classes} = this.props;
+        const steps = getSteps();
+        const {activeStep} = this.state;
+
         return(
-            <LoginNavbar/>
+            <div>
+                <LoginNavbar/>
+            
+            <div className={classes.root}>
+                <Stepper activeStep={activeStep} alternativeLabel>
+                    {steps.map(label => (
+                        <Step key={label.indexOf()}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+                <div>
+                {(() => {
+                    switch(activeStep) {
+                    //First step: Name
+                    case 0:
+                        return <div className="center-form">
+                        <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                        <TextField
+                        id="FirstNameTB"
+                        label="First Name"
+                        className={classes.textField}
+                        value={this.state.firstName}
+                        onChange={this.handleChange('firstName')}
+                        margin="normal"
+                        />
+                        <br/>
+                        <TextField
+                        id="LastNameTB"
+                        label="Last Name"
+                        className={classes.textField}
+                        value={this.state.lastName}
+                        onChange={this.handleChange('lastName')}
+                        margin="normal"
+                        />
+                        <div>
+                        <Button
+                            disabled={activeStep === 0}
+                            onClick={this.handleBack}
+                            className={classes.backButton}>
+                            Back
+                        </Button>
+                        <Button variant="contained" color="primary" onClick={this.handleNext}>
+                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                        </Button>
+                        </div>
+                    </div>;
+                    //Second step: Login credentials
+                    case 1:
+                    return "1";
+                    case steps.length:
+                    return "steps.length";
+                    default:
+                    return null;
+                    }
+                })()}
+                    {/* {this.state.activeStep === steps.length ? (
+                        <div className="center-form">
+                            <Typography className={classes.instructions}>All steps completed</Typography>
+                            <Button onClick={this.handleReset}>Reset</Button>
+                        </div>
+                    ) : (
+                        <div className="center-form">
+                            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                            <div>
+                                <Button
+                                    disabled={activeStep === 0}
+                                    onClick={this.handleBack}
+                                    className={classes.backButton}>
+                                    Back
+                                </Button>
+                                <Button variant="contained" color="primary" onClick={this.handleNext}>
+                                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                </Button>
+                            </div>
+                        </div>
+                    )} */}
+                </div>
+            </div>           
+            </div> 
         );
     }
 }
 
-export default Register;
+export default withStyles(styles)(Register);
