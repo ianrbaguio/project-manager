f0069086349930f33fae256043bb5ac5d27cf4a5
Ian Rey Baguio <ianrey_2004@yahoo.com>
2019-05-26 21:55:37 -0600

    Added text forms for AddProjectModal Component

--- src/components/AddProjectModal.js
+++ src/components/AddProjectModal.js
@@ -3,7 +3,41 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-bootstrap/Modal';
 
 class AddProjectModal extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "",
+            startDate: "",
+            userID: 0,
+            targetEndDate: ""
+        }
+
+        //bind handleChange to handle error when textbox has changed
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount(){
+        //get the logged in user
+        const user = JSON.parse(sessionStorage.getItem("LoggedInUser"));
+        const userID = user[0].UserID;
+
+        if(userID > 0){
+
+            this.setState({
+                userID: userID
+            });
+
+        }
+    };
+
+    //reference: http://jasonwatmore.com/post/2018/09/11/react-basic-http-authentication-tutorial-example#user-service-js
+    handleChange(e){
+        const {name,value} = e.target;
+        this.setState({ [name]: value});
+    }
+
     render(){
+        const {name, startDate, targetEndDate} = this.state
         return(
             <Modal
                 {...this.props}
@@ -17,7 +51,11 @@ class AddProjectModal extends React.Component{
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4>Centered Modal</h4>
+                    <div className="center">
+                         <span className="span-label">Project Name: </span> <input name="name" value={name} type="text" className="input-text" onChange={this.handleChange}/>
+                         <span className="span-label">Start Date:</span> <input name="startDate" value={startDate} type="date" className="input-text" onChange={this.handleChange}/>
+                         <span className="span-label">Target End Date:</span> <input name="targetEndDate" value={targetEndDate} type="date" className="input-text" onChange={this.handleChange}/>
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
 
