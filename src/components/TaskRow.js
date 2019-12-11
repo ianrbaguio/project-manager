284ab1b4f87a7a054e746c83ea2971197ef9fe4c
Ian Rey Baguio <ianrey_2004@yahoo.com>
2019-12-10 19:54:12 -0700

    Added behind-schedule css class

--- src/components/TaskRow.js
+++ src/components/TaskRow.js
@@ -1,73 +1,89 @@
-import React from 'react';
+import React from "react";
 //import logo from '../logo.svg';
-import '../App.css';
-import global from '../global/global.json';
+import "../App.css";
+import global from "../global/global.json";
 
-class TaskRow extends React.Component{
-    constructor(props){
-        super(props);
-        //state that sets backgroundColor to green (task is complete)
-        this.state = {
-            backgroundColor: '',
-            isChecked: false,
-        }
+class TaskRow extends React.Component {
+  constructor(props) {
+    super(props);
+    //state that sets backgroundColor to green (task is complete)
+    this.state = {
+      backgroundColor: "",
+      isChecked: false
     };
+  }
 
-    // checks if actual end date is something, then set background-color to green
-    componentDidMount(){
-        if(this.props.task.ActualEndDate !== ""){
+  // checks if actual end date is something, then set background-color to green
+  componentDidMount() {
+    let today = new Date();
+    let taskEndDate = Date.parse(this.props.task.TargetEndDate);
+    let backgroundColor = "";
+    let isChecked = false;
+
+    if (this.props.task.ActualEndDate !== "") {
+      backgroundColor = "rgba(0,255,0,0.2)";
+      isChecked = true;
+    } else if (taskEndDate < today) {
+      backgroundColor = "rgba(255,0,0,0.2)";
+      isChecked = false;
+    }
+
+    this.setState({
+      backgroundColor: backgroundColor,
+      isChecked: isChecked
+    });
+  }
+
+  //checkbox check change
+  handleChange = event => {
+    //if it's checked, then disable checkbox and set background-color to green
+    if (event.target.checked) {
+      const taskID = this.props.task.TaskID;
+      event.target.disabled = true;
+      event.target.checked = true;
+
+      fetch(global.api + "/webService/completeTask?taskID=" + taskID)
+        .then(res => res.json())
+        .then(data => {
+          if (data.return) {
             this.setState({
-                backgroundColor: "rgba(0, 255, 0, 0.2)",
-                isChecked: true,
+              backgroundColor: "rgba(0, 255, 0, 0.2)",
+              isChecked: true
             });
-        }
+          }
+        })
+        .catch(error => "GetProjects Error: " + error);
     }
+  };
 
-    //checkbox check change
-    handleChange = event => {
-        // console.log("checkbox value: " + event.target.value);
-        // console.log("checkbox checked: " + event.target.checked);
-        // console.log("Actual End Date: " + this.props.task.ActualEndDate);
-
-        //if it's checked, then disable checkbox and set background-color to green
-        if(event.target.checked){
-            const taskID = this.props.task.TaskID;
-            event.target.disabled = true;
-            event.target.checked = true;
-
-            fetch(global.api + "/webService/completeTask?taskID=" + taskID)
-            .then((res) => res.json())
-            .then((data) => {
-                if(data.return){
-                    this.setState({
-                        backgroundColor: "rgba(0, 255, 0, 0.2)",
-                        isChecked: true,
-                    });
-                }
-            })
-            .catch(error => "GetProjects Error: " + error);
-        }
-    }
-
-    render(){
-        const { backgroundColor, isChecked } = this.state;
-        return(
-            <div className="project-task-row" style={{backgroundColor: backgroundColor}}>
-                <div className="project-column project-hide-show-header">
-                    <input type="checkbox" value={this.props.task.TaskID} onChange={this.handleChange} checked={isChecked} disabled={isChecked} />
-                </div>
-                <div className="project-column project-name">
-                    {this.props.task.TaskName}
-                </div>
-                <div className="project-column project-start-date">
-                    {this.props.task.StartDate}
-                </div>
-                <div className="project-column project-end-date">
-                    {this.props.task.TargetEndDate}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { backgroundColor, isChecked } = this.state;
+    return (
+      <div
+        className="project-task-row"
+        style={{ backgroundColor: backgroundColor }}
+      >
+        <div className="project-column project-hide-show-header">
+          <input
+            type="checkbox"
+            value={this.props.task.TaskID}
+            onChange={this.handleChange}
+            checked={isChecked}
+            disabled={isChecked}
+          />
+        </div>
+        <div className="project-column project-name">
+          {this.props.task.TaskName}
+        </div>
+        <div className="project-column project-start-date">
+          {this.props.task.StartDate}
+        </div>
+        <div className="project-column project-end-date">
+          {this.props.task.TargetEndDate}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TaskRow;
--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -1,57 +1,50 @@
-import React from 'react';
+import React from "react";
 //import logo from '../logo.svg';
-import TaskRow from './TaskRow';
-import '../App.css';
-import global from '../global/global.json';
+import TaskRow from "./TaskRow";
+import "../App.css";
+import global from "../global/global.json";
 
-class TaskTable extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            Tasks: [],
-            isMounted: false
+class TaskTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Tasks: [],
+      isMounted: false
+    };
+  }
+  componentDidMount() {
+    this.setState({ isMounted: true }, function() {
+      if (this.state.isMounted) {
+        var projectID = this.props.projectID;
+        if (projectID > 0) {
+          fetch(global.api + "/webService/getTasks?projectID=" + projectID)
+            .then(res => res.json())
+            .then(data => {
+              this.setState({
+                Tasks: data.tasks
+              });
+            })
+            .catch(error => "Get Tasks Error: " + error);
         }
-        
-    }
-    componentDidMount(){
+      }
+    });
+  }
 
-        this.setState({isMounted: true}, function(){
-            if(this.state.isMounted){
-                var projectID = this.props.projectID;
-                console.log(projectID);
-                if(projectID > 0){
-                    fetch(global.api + "/webService/getTasks?projectID=" + projectID)
-                    .then(res => res.json())
-                    .then((data) => {
-                
-                    this.setState({
-                        Tasks: data.tasks
-                    });
-                    })
-                    .catch(error => "Get Tasks Error: " + error);
-                }
-            }
-        })
-    }
+  componentWillUnmount() {
+    this.setState({ isMounted: false });
+  }
 
-    componentWillUnmount(){
-        console.log("HERE");
-        this.setState({isMounted: false});
-    }
-
-    render(){
-        const {Tasks} = this.state;
-        return(
-            <div>
-                {
-                Tasks.map((task) => {
-                    return(
-                        <TaskRow task={task} key={task.TaskID} />
-                    );
-                })}
-            </div>
-        );
-    }
+  render() {
+    const { Tasks } = this.state;
+    return (
+      <div>
+        {Tasks.map(task => {
+          let today = new Date();
+          return <TaskRow task={task} key={task.TaskID} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default TaskTable;
